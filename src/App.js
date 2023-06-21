ae9621ba41fccc891291f4240a3ea958f6cce9f0
Brayan <becg852456@gmail.com>
2023-06-20 22:49:37 -0500

    on change en un input

--- src/App.js
+++ src/App.js
@@ -4,66 +4,32 @@ import { useState } from 'react';
 
 function App() {
 
-  const [datos,setDatos] = useState(
-    {
-      nombre:'',
-      edad:'',
-      estudios:false
+  const [numero,setNumero] = useState('');
+
+
+
+  function bloquearNumeros(event) {
+
+    const entrada = event.target.value;
+    let cantidad = 0;
+    for(let i = 0;i<entrada.length;i++){
+      if(entrada[i] == '0' || entrada[i] == '1'){
+        cantidad++;
+      }
+      if(cantidad == entrada.length){
+        setNumero(entrada)
+      }
     }
-  );
 
-  function cambiarNombre(e){
-    setDatos((valores) => ({
-      ...valores,
-      nombre:e.target.value
-    }));
+
   }
 
-  function cambiarEdad(e){
-    setDatos((valores) => ({
-      ...valores,
-      edad:e.target.value
-    }));
-  }
 
-  function cambiarEstudios(e){
-    setDatos((valores) => ({
-      ...valores,
-      estudios: !datos.estudios
-    }));
-  }
-
-function procesar(e){
-  e.preventDefault();
-  alert(
-    datos.nombre+"  "+datos.edad+"  "+datos.estudios
-  );
-}
+  
 
   return (
     <div >
-      <form onSubmit={procesar}> 
-      <div>
-        <p>Ingrese su nombre:</p>
-        <input type='text' value={datos.nombre} onChange={cambiarNombre}/>
-        </div>
-        <div>
-        <p>Ingrese su Edad:</p>
-        <input type='text' value={datos.edad} onChange={cambiarEdad}/>
-        </div>
-        <div>
-        <p>Estudios:</p>
-        <input type='checkbox' value={datos.estudios} onChange={cambiarEstudios}/>
-        </div>
-        <p>
-          <input type='submit'/> 
-        </p>
-      </form>
-      <hr/>
-      <h3>Datos ingresados</h3>
-      <p>Nombre: {datos.nombre}</p>
-      <p>Edad: {datos.edad}</p>
-      <p>Estudios: {datos.estudios ? 'Con estudios' : 'Sin estudios'}</p>
+      <input type='number' value={numero} onChange={bloquearNumeros} />
     </div>
   );
 }
