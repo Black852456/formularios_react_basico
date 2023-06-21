6042e3680b748e169ae92812d29bcabb84ddeec6
Brayan <becg852456@gmail.com>
2023-06-20 22:04:35 -0500

    Conceptos basicos de los eventos en el formulario basico

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [datos,setDatos] = useState(
+    {
+      nombre:'',
+      edad:'',
+      estudios:false
+    }
+  );
+
+  function cambiarNombre(e){
+    setDatos((valores) => ({
+      ...valores,
+      nombre:e.target.value
+    }));
+  }
+
+  function cambiarEdad(e){
+    setDatos((valores) => ({
+      ...valores,
+      edad:e.target.value
+    }));
+  }
+
+  function cambiarEstudios(e){
+    setDatos((valores) => ({
+      ...valores,
+      estudios: !datos.estudios
+    }));
+  }
+
+function procesar(e){
+  e.preventDefault();
+  alert(
+    datos.nombre+"  "+datos.edad+"  "+datos.estudios
+  );
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div >
+      <form onSubmit={procesar}> 
+      <div>
+        <p>Ingrese su nombre:</p>
+        <input type='text' value={datos.nombre} onChange={cambiarNombre}/>
+        </div>
+        <div>
+        <p>Ingrese su Edad:</p>
+        <input type='text' value={datos.edad} onChange={cambiarEdad}/>
+        </div>
+        <div>
+        <p>Estudios:</p>
+        <input type='checkbox' value={datos.estudios} onChange={cambiarEstudios}/>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type='submit'/> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </form>
+      <hr/>
+      <h3>Datos ingresados</h3>
+      <p>Nombre: {datos.nombre}</p>
+      <p>Edad: {datos.edad}</p>
+      <p>Estudios: {datos.estudios ? 'Con estudios' : 'Sin estudios'}</p>
     </div>
   );
 }
